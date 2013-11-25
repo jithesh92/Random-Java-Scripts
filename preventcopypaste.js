e9e731a4b9c0cab933b3e4195683cde091c99523
Jithesh Vijayakumar <jithesh.92@gmail.com>
2013-11-25 20:41:46 +0530

    Copy Paste Prevention

---
+++ preventcopypaste.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+document.oncopy = function(){
+    var bodyEl = document.body;
+    var selection = window.getSelection();
+    selection.selectAllChildren( document.createElement( 'div' ) );
+};
+</script>
